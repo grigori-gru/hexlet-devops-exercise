e5ac332c1099c81c76d7084e95c27c33f729a146
Grigori Grudinin <grgr14@mail.ru>
2021-05-15 15:59:44 +0300

    add database_url for orm

--- src/modules/config/configuration.ts
+++ src/modules/config/configuration.ts
@@ -1,12 +1,17 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 
-export default (): { database: TypeOrmModuleOptions } => ({
-    database: {
-        type: 'postgres',
-        host: process.env.DATABASE_HOST,
-        port: Number(process.env.DATABASE_PORT),
-        username: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PASSWORD,
-        database: process.env.POSTGRES_DB,
-    },
-});
+export default (): { database: PostgresConnectionOptions } => {
+    const extraOptions = process.env.DATABASE_URL
+        ? {
+              url: process.env.DATABASE_URL,
+          }
+        : {
+              host: process.env.DATABASE_HOST,
+              port: Number(process.env.DATABASE_PORT),
+              username: process.env.POSTGRES_USER,
+              password: process.env.POSTGRES_PASSWORD,
+              database: process.env.POSTGRES_DB,
+          };
+
+    return { database: { type: 'postgres', ...extraOptions } };
+};
